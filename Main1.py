58cef2ee77fc93bf61d50a60168a4b09af8b79af
codecherry12 <jeeiit.middeguru2000@gmail.com>
2021-07-12 16:25:10 +0530

    added modules and functions

---
+++ Main1.py
@@ -0,0 +1,8 @@
+from sys import path
+path.append("D:\\programs\\Packages\\3105")
+import Module
+
+zeroes = [0 for i in range(5)]
+ones = [1 for i in range(5)]
+print(Module.suml(zeroes))
+print(Module.prodl(ones))
